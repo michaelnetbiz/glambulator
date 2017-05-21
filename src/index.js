ee68720d50b1d7c625beaf11071af44e37b5aa0f
Michael Nelson <michael.nelson@fulbrightmail.org>
2017-05-21 18:52:13 -0400

    remove store subscription to state cached in browser storage

--- src/index.js
+++ src/index.js
@@ -4,26 +4,22 @@ import React from "react";
 import {Provider} from "react-redux";
 import ReactDOM from "react-dom";
 import injectTapEventPlugin from "react-tap-event-plugin";
-import configureStore from "./glambulApp/store/configureStore";
-import saveState from "./glambulApp/store/saveState";
+import configureStore from "./app/store/configureStore";
 import {DEFAULT} from "./sparqlQuery/Expressions";
 import {setSparqlQuery} from "./sparqlQuery/sparqlQueryActions";
 import {executeReferenceModelRequest} from "./referenceModel/referenceModelActions";
-import GlambulApp from "./glambulApp/containers/GlambulApp";
+import App from "./app/containers/App";
 import "./styles/style.scss";
 
 injectTapEventPlugin();
 
 const store = configureStore();
-store.subscribe(() => {
-  saveState(store.getState());
-});
 store.dispatch(setSparqlQuery(DEFAULT));
 store.dispatch(executeReferenceModelRequest());
 
 ReactDOM.render(
   <Provider store={store}>
-    <GlambulApp/>
+    <App/>
   </Provider>,
   document.getElementById("root")
 );
