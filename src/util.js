ab988fcb2fe2800ad1457af6a8889f604a9db1be
Michael Nelson <michael.nelson@fulbrightmail.org>
2017-05-21 18:52:51 -0400

    abstract utils from modules

--- src/util.js
+++ src/util.js
@@ -1,11 +1,10 @@
 // @flow
-
 import {scaleOrdinal} from "d3-scale";
 import * as colors from "material-ui/styles/colors";
 
-export const colorScheme = scaleOrdinal([
-  colors.red600,
+const palette = [
   colors.indigo600,
+  colors.red600,
   colors.teal600,
   colors.yellow600,
   colors.grey600,
@@ -23,7 +22,9 @@ export const colorScheme = scaleOrdinal([
   colors.green600,
   colors.amber600,
   colors.brown600
-]);
+];
+
+export const colorScheme = scaleOrdinal(palette);
 
 export const makeHash = (str: string) => {
   let chr, hash, i;
@@ -47,4 +48,29 @@ export const normalizeString = (str: string) => {
     .replace(/_/g, "")
     .replace(/#/g, "")
     .toLowerCase();
+};
+
+export const groupExpressions = {
+  "britishMuseum": "http:\/\/collection\.britishmuseum\.org\/id\/.*",
+  "erlangen": "http:\/\/erlangen-crm\.org\/current\/.*",
+  "rdf": "http:\/\/www\.w3\.org\/1999\/02\/22\-rdf\-syntax\-ns\#.*",
+  "rdfs": "http:\/\/www\.w3\.org\/2000\/01\/rdf\-schema\#.*",
+  "skos": "http:\/\/www\.w3\.org\/2004\/02\/skos\/core\#.*",
+  "owl": "http:\/\/www\.w3\.org\/2002\/07\/owl\#.*"
+};
+
+export const classExpressions = {
+  "bibliographicObject": "http:\/\/collection\.britishmuseum\.org\/id\/bibliography\/.*",
+  "collectionObject": "http:\/\/collection\.britishmuseum\.org\/id\/.*",
+  "department": "http:\/\/collection\.britishmuseum\.org\/id\/department\/.*",
+  "dimension": "http:\/\/collection\.britishmuseum\.org\/id\/dimension\/.*",
+  "modelExtension": "http:\/\/collection\.britishmuseum\.org\/id\/ontology\/.*",
+  "ontologyResource": "http:\/\/erlangen-crm\.org\/current\/.*",
+  "owlResource": "http:\/\/www\.w3\.org\/2002\/07\/owl\#.*",
+  "personOrInstitution": "http:\/\/collection\.britishmuseum\.org\/id\/person-institution\/.*",
+  "rdfSchemaEntity": "http:\/\/www\.w3\.org\/2000\/01\/rdf\-schema\#.*",
+  "rdfSyntaxEntity": "http:\/\/www\.w3\.org\/1999\/02\/22\-rdf\-syntax\-ns\#.*",
+  "skosTerm": "http:\/\/www\.w3\.org\/2004\/02\/skos\/core\#.*",
+  "term": "http:\/\/collection\.britishmuseum\.org\/id\/thesauri\/.*",
+  "unit": "http:\/\/collection\.britishmuseum\.org\/id\/units\/.*"
 };
