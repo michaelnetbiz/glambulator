508637f593c6b238eca9e8d7a13be07e166dbccd
Michael Nelson <michael.nelson@fulbrightmail.org>
2017-05-25 00:10:52 -0400

    update NavDrawer component

--- src/common/components/NavDrawer.js
+++ src/common/components/NavDrawer.js
@@ -8,10 +8,11 @@ import BuildIcon from "material-ui/svg-icons/action/build";
 import CodeIcon from "material-ui/svg-icons/action/code";
 import OpenInNewIcon from "material-ui/svg-icons/action/open-in-new";
 import PaletteIcon from "material-ui/svg-icons/image/palette";
+import BookIcon from "material-ui/svg-icons/action/book";
 import CopyrightIcon from "material-ui/svg-icons/action/copyright";
 import {groupExpressions} from "../../util";
 import EntityGroupChip from "../../entity/components/EntityGroupChip";
-import ccIcon from "../../assets/by-nc-sa.svg";
+import byNcSaIcon from "../../assets/by-nc-sa.svg";
 
 const NavDrawer = (params: Object) => {
   const {
@@ -30,29 +31,78 @@ const NavDrawer = (params: Object) => {
       open={isNavDrawerOpen}
       width={280}
     >
-      <List>
+      <List
+        className={"navDrawerList"}
+      >
         <ListItem
+          className={"navDrawerListItem"}
           leftIcon={
             <InfoOutlineIcon color={colors.teal400}/>
           }
           nestedItems={[
             <ListItem
-              disabled
+              children={"Galleries, libraries, archives, and museums (GLAM)"}
+              href={"//en.wikipedia.org/wiki/GLAM_(industry_sector)"}
               key={0}
+            />,
+            <ListItem
+              children={"\u002B"}
+              disabled
+              key={1}
+              style={{
+                "textAlign": "center"
+              }}
+            />,
+            <ListItem
+              href={"//www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.04.0059:entry=ambulator"}
+              key={2}
             >
-              {"The application was created over the span of the fellowship. Big ideas include: material design, cidoc crm, and exploring the possibilities of semantic web applications for the heritage space. In this paper i discuss motivations, product- the web app; process; implications; and future directions/room for improvement."}
-            </ListItem>
+              <span>{"Ambulator"}</span>
+              <span>&nbsp;</span>
+              <span>{"\u2022"}</span>
+              <span>&nbsp;</span>
+              <i>{"Noun"}</i>
+              <span>&nbsp;</span>
+              <span>{"\u2022"}</span>
+              <span>&nbsp;</span>
+              <span>{"\"one that walks about\""}</span>
+              <span>&nbsp;</span>
+              <span>{"\u2022"}</span>
+              <span>&nbsp;</span>
+              <span>{"(Lewis & Short, 1879)"}</span>
+            </ListItem>,
+            <ListItem
+              children={"\u003D"}
+              disabled
+              key={3}
+              style={{
+                "textAlign": "center"
+              }}
+            />,
+            <ListItem
+              children={
+                <div
+                  key={0}
+                >
+                  <span>{"Glambulator, an application for:"}</span>
+                  <ul>
+                    <li>{"exploring the CIDOC conceptual reference model (CRM) for heritage documentation"}</li>
+                    <li>{"executing Sparql queries against the British Museum’s Semantic Web Collection Online"}</li>
+                    <li>{"browsing graphs of the resultant RDF triples"}</li>
+                  </ul>
+                </div>
+              }
+              disabled
+              key={4}
+            />
           ]}
           primaryText={"About"}
           primaryTogglesNestedList
         />
         <ListItem
           children={"Colors"}
-          leftIcon={
-            <PaletteIcon
-              color={colors.teal400}
-            />
-          }
+          className={"navDrawerListItem"}
+          leftIcon={<PaletteIcon color={colors.teal400}/>}
           nestedItems={
             Object.keys(groupExpressions).map((k, ix) => {
               return (
@@ -70,24 +120,18 @@ const NavDrawer = (params: Object) => {
               );
             })
           }
+          primaryTogglesNestedList
         />
         <ListItem
+          className={"navDrawerListItem"}
           insetChildren
-          leftIcon={
-            <BuildIcon
-              color={colors.teal400}
-            />
-          }
+          leftIcon={<BuildIcon color={colors.teal400}/>}
           nestedItems={[
             <ListItem
-              href={"//michaelnet.biz"}
+              href={""}
               key={0}
-              style={{
-                "fontSize": "0.75em",
-                "textAlign": "left"
-              }}
             >
-              {"Introductory Blog Post "}
+              {"Blog "}
               <OpenInNewIcon
                 color={colors.teal400}
                 style={{
@@ -99,12 +143,8 @@ const NavDrawer = (params: Object) => {
               />
             </ListItem>,
             <ListItem
-              href={"//michaelnet.biz"}
+              href={""}
               key={1}
-              style={{
-                "fontSize": "0.75em",
-                "textAlign": "left"
-              }}
             >
               {"White Paper "}
               <OpenInNewIcon
@@ -122,12 +162,9 @@ const NavDrawer = (params: Object) => {
           primaryTogglesNestedList
         />
         <ListItem
+          className={"navDrawerListItem"}
           href={"//github.com/michaelnetbiz/glambulator"}
-          leftIcon={
-            <CodeIcon
-              color={colors.teal400}
-            />
-          }
+          leftIcon={<CodeIcon color={colors.teal400}/>}
         >
           {"Source "}
           <OpenInNewIcon
@@ -141,19 +178,76 @@ const NavDrawer = (params: Object) => {
           />
         </ListItem>
         <ListItem
-          disabled
-          leftIcon={
-            <CopyrightIcon
-              color={colors.teal400}
-            />
-          }
+          className={"navDrawerListItem"}
+          leftIcon={<CopyrightIcon color={colors.teal400}/>}
           nestedItems={[
             <ListItem
-              className={"navDrawerNestedListItem"}
-              href={"//creativecommons.org/licenses/by-nc-sa/4.0/"}
+              key={0}
+              primaryText={
+                <span>
+                  <span>
+                    {"Aside from CIDOC-CRM, the data, text and images available via this service are licensed under the "}
+                  </span>
+                  <a href={"//creativecommons.org/licenses/by-nc-sa/4.0/"}>
+                    {"Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)"}
+                    <OpenInNewIcon
+                      color={colors.teal400}
+                      style={{
+                        "height": "16px",
+                        "left": "7px",
+                        "top": "0px",
+                        "width": "16px"
+                      }}
+                    />
+                  </a>
+                  <span>
+                    {" license."}
+                  </span>
+                </span>
+              }
+            />,
+            <ListItem
+              disabled
+              key={1}
+            >
+              <span>
+                <span>{"The application was designed and developed by "}</span>
+                <a href="//michaelnet.biz">
+                  {"Michael E. Nelson"}
+                  <OpenInNewIcon
+                    color={colors.teal400}
+                    style={{
+                      "height": "16px",
+                      "left": "7px",
+                      "top": "0px",
+                      "width": "16px"
+                    }}
+                  />
+                </a>
+                <span>{", as part of the Cultural Heritage Informatics (CHI) Initiative Graduate Fellowship at Michigan State University."}</span>
+            </span>
+            </ListItem>,
+            <ListItem
+              children={
+                <img
+                  src={byNcSaIcon}
+                />
+              }
+              disabled
+              key={2}
+            />
+          ]}
+          primaryText={"Licensing"}
+          primaryTogglesNestedList
+        />
+        <ListItem
+          leftIcon={<BookIcon color={colors.teal400}/>}
+          nestedItems={[
+            <ListItem
+              href={"//collection.britishmuseum.org/"}
               key={0}
             >
-              {"License."}
+              {"The British Museum Semantic Web Collection"}
               <OpenInNewIcon
                 color={colors.teal400}
                 style={{
@@ -165,37 +259,23 @@ const NavDrawer = (params: Object) => {
               />
             </ListItem>,
             <ListItem
-              children={
-                <span
-                  key={0}
-                >
-                  <img
-                    src={ccIcon}
-                  />
-                  <div
-                    style={{
-                      "fontSize": ".75em"
-                    }}
-                  >
-                    {"© Trustees of the British Museum."}
-                    <OpenInNewIcon
-                      color={colors.teal400}
-                      style={{
-                        "height": "16px",
-                        "left": "7px",
-                        "top": "0px",
-                        "width": "16px"
-                      }}
-                    />
-                  </div>
-                </span>
-              }
-              className={"navDrawerNestedListItem"}
-              href={"//collection.britishmuseum.org/licensing.html"}
+              href={"//www.cidoc-crm.org/"}
               key={1}
-            />
+            >
+              {"CIDOC-CRM"}
+              <OpenInNewIcon
+                color={colors.teal400}
+                style={{
+                  "height": "16px",
+                  "left": "7px",
+                  "top": "0px",
+                  "width": "16px"
+                }}
+              />
+            </ListItem>
           ]}
-          primaryText={"Licensing"}
+          primaryText={"Further Reading"}
+          primaryTogglesNestedList
         />
       </List>
     </Drawer>
